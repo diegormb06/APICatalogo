cf18293499681ba388df5674fcf119a53a6f558c
Diego Rodrigues <diegormb@live.com>
2023-01-27 21:54:36 -0300

    first commit

---
+++ Domain/entities/Categoria.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Domain;
+
+public class Categoria
+{
+  public Categoria()
+  {
+    Produtos = new Collection<Produto>();
+  }
+
+  public int Id { get; set; }
+  public string? Nome { get; set; }
+  public string? ImgagemUrl { get; set; }
+  public ICollection<Produto>? Produtos { get; set; }
+}
---
+++ Domain/entities/Produto.cs
@@ -0,0 +1,14 @@
+﻿namespace APICatalogo.Domain;
+
+public class Produto
+{
+  public int Id { get; set; }
+  public string? Nome { get; set; }
+  public string? Descricao { get; set; }
+  public decimal Preco { get; set; }
+  public string? ImagemUrl { get; set; }
+  public float Estoque { get; set; }
+  public DateTime DataCadastro { get; set; }
+  public int CategoriaId { get; set; }
+  public Categoria? Categoria { get; set; }
+}
